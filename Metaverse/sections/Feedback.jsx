f2a2f610515f418bc5413a0f2f549af61dffcf5b
hasnainkhagan <official.thvm@gmail.com>
2024-06-26 04:05:33 +0500

    metaverse

--- Metaverse/sections/Feedback.jsx
+++ Metaverse/sections/Feedback.jsx
@@ -24,7 +24,7 @@ const Feedback = () => (
             Hasnain Khan
           </h4>
           <p className="mt-[8px] font-normal sm:text-[18px] text-[12px] sm:leading-[22.68px] leading-[16.68px] text-white">
-            Founder of Metaverus
+            Founder of Metaverse
           </p>
         </div>
 
--- Metaverse/sections/World.jsx
+++ Metaverse/sections/World.jsx
@@ -15,7 +15,7 @@ const World = () => (
       viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto flex flex-col`}
     >
-      <TypingText title="| People on the World" textStyles="text-center" />
+      <TypingText title="| People in the World" textStyles="text-center" />
       <TitleText
         title={
           <>
